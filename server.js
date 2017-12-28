3081dfce857e05bddbb8dae03b5ec105133d33e0
Peter McCready <petermccready2@gmail.com>
2017-12-28 13:07:09 +0000

    adds connection to mongoDB

--- server.js
+++ server.js
@@ -2,14 +2,21 @@ var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
 var path = require('path');
+var MongoClient = require('mongodb').MongoClient;
+var ObjectId = require('mongodb').ObjectId;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static('client/build'));
 // app.use(require('./controllers/index'));
 
-app.listen(3000, function(){
-  console.log('App listening on ' + this.address().port);
+MongoClient.connect('mongodb://localhost:27017/snow_resorts', function(err, client){
+  if (err){ return console.log(err); }
+  db = client.db('snow_resorts');
+  console.log('Connected to DB');
+  app.listen(3000, function(){
+    console.log('App listening on ' + this.address().port);
+  });
 });
 
 app.get('/', function(req, res){
