d83f3779785ccff965025147b4715ba164056ffa
Peter McCready <petermccready2@gmail.com>
2017-12-28 13:47:49 +0000

    adds basic google map centered on Scotland level

--- client/src/app.js
+++ client/src/app.js
@@ -1,5 +1,15 @@
+var MapWrapper = require('./models/mapWrapper');
+
+var createMap = function(){
+  var container = document.getElementById('map-container');
+  var center = { lat: 56.740674, lng: -4.2187500 };
+  var zoom = 7;
+  var map = new MapWrapper(container, center, zoom);
+}
+
 var app = function(){
-  console.log("app running");
+  console.log('app running');
+  createMap();
 };
 
 window.addEventListener('load', app);
---
+++ client/src/models/mapWrapper.js
@@ -0,0 +1,9 @@
+var MapWrapper = function(container, coords, zoom){
+  this.googleMap = new google.maps.Map(container, {
+    center: coords,
+    zoom: zoom
+  });
+  this.markers = [];
+}
+
+module.exports = MapWrapper;
